856051b583d302d6a538bdace295c16fc1579a6d
StoicJHS <yh.nam.stoic@gmail.com>
2025-10-14 14:51:07 +0900

    2025-10-14 14:51

--- chap4-node.js/4-3-access-external-data/h4.14.js
+++ chap4-node.js/4-3-access-external-data/h4.14.js
@@ -47,4 +47,14 @@ async function main(){
     403 = Forbidden. 엑세스가 금지.
     500 = Internal Server Error. 서버 내부에서 에러 발생.
 
+    위에서 데이터의 송신에 'PUT'이라는 HTTP 메소드 사용. 
+    WEB서버에 엑세스할 때는, HTTP라는 프로토콜을 사용.
+    보통 데이터를 습득할 때는 GET 메소드 사용. 
+    POST: 폼을 서버에 송신하는 경우에 사용. 
+
+    hostname: 엑세스하는 서버명
+    path: 서버내 파일등 디렉토리
+    method: 메소드명
+    headers: 헤더 정보. 
+
 */
